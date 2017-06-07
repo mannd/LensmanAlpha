59675a96bb99e28c21b027f34899971a2cf7bb3e
David Mann <mannd@epstudiossoftware.com>
2017-06-06 20:19:42 -0600

    Playing with animation of view background

--- LensmanAlpha/ViewController.swift
+++ LensmanAlpha/ViewController.swift
@@ -10,10 +10,18 @@ import UIKit
 
 class ViewController: UIViewController {
     @IBOutlet var mainStackView: UIStackView!
+    @IBOutlet var statusView: UIView!
+    @IBOutlet var commandView: UIView!
+    @IBOutlet var screensView: UIView!
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        // test animation
+        flashView(view: statusView)
+        RunAfterDelay(5, block: {
+            self.endFlashView(view: self.statusView, delay: 0.0, color: .red) })
     }
 
     override func didReceiveMemoryWarning() {
@@ -29,6 +37,25 @@ class ViewController: UIViewController {
         }
         mainStackView.layoutSubviews()
         mainStackView.setNeedsDisplay()
+    }
+    
+    func flashView(view: UIView){
+        UIView.animate(withDuration: 1.0, delay: 0.0, options:[.repeat, .autoreverse], animations: {
+            view.backgroundColor = .white
+        }, completion:nil)
+    }
+    
+    func endFlashView(view: UIView, delay: Double, color: UIColor) {
+        
+        UIView.animate(withDuration: 0.1, delay: delay, options:[.beginFromCurrentState], animations: {
+            view.backgroundColor = color }, completion:nil)
+    }
+    
+    func RunAfterDelay(_ delay: TimeInterval, block: @escaping ()->())
+    {
+        let time = DispatchTime.now() + Double(Int64(delay * Double(NSEC_PER_SEC))) / Double(NSEC_PER_SEC)
+        
+        DispatchQueue.main.asyncAfter(deadline: time, execute: block)
     }
 
 
